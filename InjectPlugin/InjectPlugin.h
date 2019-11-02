436b076d22ff8065ae71dfcf8b86dba3d9046b5d
Georgiy0 <kubrin.eg.s@gmail.com>
2019-11-02 18:23:45 +0000

    Removed InjectionContext logic from InjectionPlugin because injdrv holds contextes in KM.

--- InjectPlugin/InjectPlugin.h
+++ InjectPlugin/InjectPlugin.h
@@ -6,37 +6,8 @@ typedef enum {
 	CallbackProcessCreate,
 	CallbackProcessExit,
 	CallbackThreadCreate,
+	CallbackApcProcessInject,
 } CALLBACK_ID;
-
-class InjectContext
-{
-public:
-	InjectContext(ILogger* logger, int PID, std::string imageName);
-	
-	// getters setters
-	void setInjected() { this->injected = true; }
-	bool wasInjected() { return this->injected; }
-	void setTID(int TID) { this->apcInfo.TID = TID; }
-	int getTID() { return this->apcInfo.TID; }
-#ifdef _WIN64
-	bool getWow64() { return this->Wow64; }
-#endif
-	void* getApcInfoPtr() { return &this->apcInfo; }
-
-	
-
-private:
-	APC_INFO apcInfo; // holds the buffer that will be passed to KM
-
-	std::string imageName;
-	ILogger* logger;
-	bool injected;
-#ifdef _WIN64
-	bool Wow64;
-#endif
-};
-
-typedef std::map<int, InjectContext*> ProcessMap;
 
 class AVInject : public IPlugin
 {
@@ -58,5 +29,4 @@ private:
 	IConfig* configManager;
 	ILogger* logger;
 	
-	ProcessMap processMap;
 };
